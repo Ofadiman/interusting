67f8ef7354c1cadd08b4af646e61ce72bd7f3790
ofadiman <ofadiman@gmail.com>
2024-11-15 15:25:16 +0100

    feat(docgen): read root directory

--- docgen/src/main.rs
+++ docgen/src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, World!");
+    let current_dir = env::current_dir().expect("reading current directory must not fail");
+    let root_dir = current_dir
+        .parent()
+        .expect("parent directory must be defined");
+
+    println!("{}", root_dir.display())
 }
