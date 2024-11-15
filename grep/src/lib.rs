592afab6f0f77ddd0ef22838d441796128938d9f
ofadiman <ofadiman@gmail.com>
2024-11-15 10:22:37 +0100

    refactor(grep): improve project modularity

---
+++ grep/src/lib.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config<'a> {
+    pub query: &'a String,
+    pub file_path: &'a String,
+}
+
+impl<'a> Config<'a> {
+    pub fn build(args: &'a Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        return Ok(Config {
+            query: &args[1],
+            file_path: &args[2],
+        });
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,15 +1,20 @@
+use grep;
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = grep::Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect(&format!("failed to read {file_path}"));
-    println!("{contents:?}");
+    if let Err(e) = grep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
 }
