e4a336a8a11ca62bf90bebcd888fb7ddca46725d
ofadiman <ofadiman@gmail.com>
2024-11-15 15:48:05 +0100

    feat(docgen): read directories used for documentation generation

--- docgen/src/main.rs
+++ docgen/src/main.rs
@@ -1,10 +1,35 @@
-use std::env;
+use std::{
+    env,
+    fs::{self},
+};
 
 fn main() {
-    let current_dir = env::current_dir().expect("reading current directory must not fail");
+    let current_dir = env::current_dir().expect("reading current dir must not fail");
     let root_dir = current_dir
         .parent()
-        .expect("parent directory must be defined");
+        .expect("getting parent dir must not fail");
 
-    println!("{}", root_dir.display())
+    let mut projects: Vec<String> = vec![];
+    let root_dir_paths = fs::read_dir(root_dir).expect("reading root dir must not fail");
+
+    for dir_entry_result in root_dir_paths {
+        if let Ok(dir_entry) = dir_entry_result {
+            if dir_entry
+                .metadata()
+                .expect("getting file metadata must not fail")
+                .is_dir()
+            {
+                let file_name = dir_entry
+                    .file_name()
+                    .into_string()
+                    .expect("parsing os string into string must not fail");
+
+                if !file_name.contains(".git") {
+                    projects.push(file_name);
+                }
+            }
+        }
+    }
+
+    println!("{projects:#?}");
 }
