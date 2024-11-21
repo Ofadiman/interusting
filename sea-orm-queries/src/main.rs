47729eb2e3c7b94698b8625a0c9f59f5cdb074e5
ofadiman <ofadiman@gmail.com>
2024-11-21 17:21:49 +0100

    feat(sea-orm-queries): create database migrations

--- sea-orm-queries/src/main.rs
+++ sea-orm-queries/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use sea_orm::{Database, DbErr};
+
+const DATABASE_URL: &str = "postgres://user:password@localhost:5432/postgres";
+
+async fn run() -> Result<(), DbErr> {
+    let _db = Database::connect(DATABASE_URL).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = run().await {
+        panic!("{}", err);
+    }
 }
