323af4e24eafbdf12a2913bde44ae1c714503fb9
ofadiman <ofadiman@gmail.com>
2024-11-22 10:32:34 +0100

    feat(sea-orm-queries): create users table

--- sea-orm-queries/migration/src/lib.rs
+++ sea-orm-queries/migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20241121_000001_create_bakery_table;
 mod m20241121_000002_create_chef_table;
+mod m20241122_085421_create_users;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20241121_000001_create_bakery_table::Migration),
             Box::new(m20241121_000002_create_chef_table::Migration),
+            Box::new(m20241122_085421_create_users::Migration),
         ]
     }
 }
---
+++ sea-orm-queries/migration/src/m20241122_085421_create_users.rs
@@ -0,0 +1,46 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Users::Table)
+                    .col(ColumnDef::new(Users::Id).uuid().not_null().primary_key())
+                    .col(ColumnDef::new(Users::FirstName).string().not_null())
+                    .col(ColumnDef::new(Users::LastName).string().not_null())
+                    .col(
+                        ColumnDef::new(Users::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)),
+                    )
+                    .col(
+                        ColumnDef::new(Users::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+    FirstName,
+    LastName,
+    CreatedAt,
+    UpdatedAt,
+}
